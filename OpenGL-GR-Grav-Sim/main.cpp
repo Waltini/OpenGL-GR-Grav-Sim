200030ec67eb5b65c39beb14a3a6b2be70e02f04
Waltini <rwrussell2802@gmail.com>
2025-08-24 13:18:15 +0100

    intialised the project. Include imgui license, commented over existing secitons, updated Visual Studio directories, established GLFW window

---
+++ OpenGL-GR-Grav-Sim/main.cpp
@@ -0,0 +1,44 @@
+#include <imgui/imgui.h>
+#include <imgui/backends/imgui_impl_glfw.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+// Intialising main functions ~ Informs the compiler the functions exist pretty much
+static void glfw_error_callback(int error, const char* description);
+
+const unsigned int SCR_WIDTH = 600;
+const unsigned int SCR_HEIGHT = 800;
+
+int main(int, char**)
+{
+	// Intialising GLFW
+	if (!glfwInit())
+	{
+		return -1;
+	};
+	// Context
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Informs GLFW I'm using OpenGL Version 3.x
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // Informs GLFW I'm using OpenGL Version x.3
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// GLFW functions
+	glfwSetErrorCallback(glfw_error_callback);
+	
+	// Window Creation
+	GLFWwindow* window = glfwCreateWindow(SCR_HEIGHT, SCR_WIDTH, "Hello World!!!", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+};
+
+// Function definitions ~ kept below the main loop for formatting
+static void glfw_error_callback(int error, const char* description)
+{
+	fprintf(stderr, "GLFW ERROR %d: %s\n", error, description);
+}
