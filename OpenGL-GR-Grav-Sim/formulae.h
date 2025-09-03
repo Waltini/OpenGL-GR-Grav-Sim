aa37ae07edaa4851cc3fdcdd40d09245f30076f3
Waltini <rwrussell2802@gmail.com>
2025-09-03 15:03:36 +0100

    correcting branch merge errors

--- OpenGL-GR-Grav-Sim/formulae.h
+++ OpenGL-GR-Grav-Sim/formulae.h
@@ -1,1 +1,7 @@
 #pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+glm::vec3 PN_acceleration(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 v1, glm::vec3 v2, float m1, float m2, bool years);
