f6dca713a69d4f223c8e7baf6c7ce0b0054a71d5
Waltini <rwrussell2802@gmail.com>
2025-10-10 10:48:31 +0100

    created shader class, included SDL2

--- OpenGL-GR-Grav-Sim/main.cpp
+++ OpenGL-GR-Grav-Sim/main.cpp
@@ -8,9 +8,10 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include "formulae.h"
+#include <SDL2/SDL_timer.h>
 #include "celestial_body_class.h"
 #include "integration.h"
+#include "shaders_c.h"
 
 #include <iostream>
 #include <cmath>
@@ -19,6 +20,7 @@
 #include <condition_variable>
 #include <mutex>
 #include <atomic>
+#include <chrono>
 #include <thread>
 
 // Intialising main functions ~ Informs the compiler the functions exist pretty much
@@ -26,8 +28,10 @@ static void glfw_error_callback(int error, const char* description);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
 
-const unsigned int SCR_WIDTH = 600;
-const unsigned int SCR_HEIGHT = 800;
+int SCR_WIDTH = 600;
+int SCR_HEIGHT = 800;
+
+const unsigned int PixelsPerUnitLength = 150;
 
 using ldvec3 = glm::tvec3<long double>;
 using ldmat43 = glm::mat<4, 3, long double>;
@@ -79,9 +83,10 @@ public:
 	}
 
 	mathState readBackBuffer() const { return backbuffer; } // returns the back buffer
+	clsState readFrontBuffer() const { return frontbuffer; } // returns the back buffer
 
-	void physicsStateUpdate(const ldmat43 state, const long double physics_time) { 
-		backbuffer.y = state; 
+	void physicsStateUpdate(const ldmat43 state, const long double physics_time) {
+		backbuffer.y = state;
 		backbuffer.physics_time = physics_time;
 	} // Updates the backbuffer with a new state and new time
 
@@ -104,7 +109,7 @@ public:
 		b2.setPos(dim_edits[2]);
 		b2.setVel(dim_edits[3]);
 	}
-	
+
 	// Debug Functions
 	void debugBackBuffer() const {
 		ldmat43 y = backbuffer.y;
@@ -134,7 +139,7 @@ public:
 	}
 };
 
-void physics_thread(GLFWwindow* window, RK45_integration& integrator, buffer_box& bufbx) {
+int physics_thread(GLFWwindow* window, RK45_integration& integrator, buffer_box& bufbx, float sim_speed) {
 	while (!glfwWindowShouldClose(window)) {
 		halted = true;
 		H_cv.notify_one();
@@ -153,6 +158,7 @@ void physics_thread(GLFWwindow* window, RK45_integration& integrator, buffer_box
 		if (integrator.getDebug()) {
 			if (physics_result.accepted) {
 				std::cout << "[ACCEPTED]" << std::endl;
+				std::cout << "[attempt_h] " << physics_step.attempt_h << std::endl;
 				bufbx.physicsStateUpdate(physics_result.state_y, physics_result.time_update);
 
 				std::lock_guard<std::mutex> lock(mtx);
@@ -162,23 +168,105 @@ void physics_thread(GLFWwindow* window, RK45_integration& integrator, buffer_box
 		else {
 			if (physics_result.accepted) {
 				std::cout << "[ACCEPTED]" << std::endl;
+				std::cout << "[attempt_h] " << physics_step.attempt_h << std::endl;
 				bufbx.physicsStateUpdate(physics_result.state_y, physics_result.time_update);
 
 				std::lock_guard<std::mutex> lock(mtx);
 				bufbx.changeBuffers();
 			}
 		}
-
+		
 	}
+	return 0;
 }
 
-int rendering_thread(GLFWwindow* window, int FPS, glm::vec4 background, bool show) {
+int rendering_thread(GLFWwindow* window, int FPS, glm::vec4 background, bool show, buffer_box& bufbx, const char* glsl_version) {
+	// GLFW Set
+	glfwMakeContextCurrent(window); // sets the context of the window to current on the thread
+
+	// Setup ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls    
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true); // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	// Initialises GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialise GLAD" << std::endl;
+		return -1;
+	}
+
+	Shader myShader("shader.vs", "shader.fs");
+
+	float vertices[] = {
+	-0.5f, -0.5f, -0.5f,
+	 0.5f, -0.5f, -0.5f,
+	 0.5f,  0.5f, -0.5f,
+	 0.5f,  0.5f, -0.5f,
+	-0.5f,  0.5f, -0.5f,
+	-0.5f, -0.5f, -0.5f,
+
+	-0.5f, -0.5f,  0.5f,
+	 0.5f, -0.5f,  0.5f,
+	 0.5f,  0.5f,  0.5f,
+	 0.5f,  0.5f,  0.5f,
+	-0.5f,  0.5f,  0.5f,
+	-0.5f, -0.5f,  0.5f,
+
+	-0.5f,  0.5f,  0.5f,
+	-0.5f,  0.5f, -0.5f,
+	-0.5f, -0.5f, -0.5f,
+	-0.5f, -0.5f, -0.5f,
+	-0.5f, -0.5f,  0.5f,
+	-0.5f,  0.5f,  0.5f,
+
+	 0.5f,  0.5f,  0.5f,
+	 0.5f,  0.5f, -0.5f,
+	 0.5f, -0.5f, -0.5f,
+	 0.5f, -0.5f, -0.5f,
+	 0.5f, -0.5f,  0.5f,
+	 0.5f,  0.5f,  0.5f,
+
+	-0.5f, -0.5f, -0.5f,
+	 0.5f, -0.5f, -0.5f,
+	 0.5f, -0.5f,  0.5f,
+	 0.5f, -0.5f,  0.5f,
+	-0.5f, -0.5f,  0.5f,
+	-0.5f, -0.5f, -0.5f,
+
+	-0.5f,  0.5f, -0.5f,
+	 0.5f,  0.5f, -0.5f,
+	 0.5f,  0.5f,  0.5f,
+	 0.5f,  0.5f,  0.5f,
+	-0.5f,  0.5f,  0.5f,
+	-0.5f,  0.5f, -0.5f
+	};
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// Input processing
 		process_input(window);
 		glClearColor(background.x, background.y, background.z, background.w);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Imgui Frame Start
 		ImGui_ImplOpenGL3_NewFrame();
@@ -188,9 +276,52 @@ int rendering_thread(GLFWwindow* window, int FPS, glm::vec4 background, bool sho
 		// Test GUI
 		if (show) {
 			ImGui::Begin("Hello World!");
-			ImGui::Text("Tester Text");
+			if (ImGui::Button("Pause")) {
+				pause = true;
+				P_cv.notify_one();
+			}
+			if (ImGui::Button("Unpause")) {
+				pause = false;
+				P_cv.notify_one();
+			}
 			ImGui::End();
 		}
+
+		clsState snapshot = bufbx.readFrontBuffer(); // grabs the pointers for the celestial body class objects
+		celestial_body b1 = *snapshot.b1; // De-referenced Body 1
+		celestial_body b2 = *snapshot.b2; // De-referenced Body 2
+
+		glBindVertexArray(VAO);
+
+		// view matrix, representative of the current position of the where the point of view originates and in what direction
+		glm::mat4 view = glm::mat4(1.0f);
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f)); // Sets the camera back three units
+
+		// projection matrix, responsible for dictating what is in view / what can be seen
+		glm::mat4 projection;
+		projection = glm::perspective(glm::radians(45.0f), (float)SCR_HEIGHT / (float)SCR_WIDTH, 0.1f, 100.0f); // Grabs the current screen height and width to scale display accordingly. This is responsible for converting world coordinates into NDC
+
+		myShader.use();
+		// uniform assigning
+		myShader.setMat4("view", view); // sets the calculated view matrix to the uniform variable view matrix called within the vertex shader
+		myShader.setMat4("projection", projection); // sets the calculated projection matrix to the uniform variable projection matrix called within the vertex shader
+
+		// Body 1
+		glm::mat4 model_1 = glm::mat4(1.0f);
+		b1.print();
+		glm::vec3 pos1 = (glm::vec3)b1.getPos(); // grabs the current position of Body 1
+		model_1 = glm::translate(model_1, pos1); // translates model matrix using the current position of Body 1
+		myShader.setMat4("model", model_1); // sets the calculated model matrix for Body 1 to the model matrix called within the vertex shader
+
+		glDrawArrays(GL_TRIANGLES, 0, 36);  // Updates the draw array with the current values for Body 1 set in the shader
+
+		// Body 2
+		glm::mat4 model_2 = glm::mat4(1.0f);
+		glm::vec3 pos2 = (glm::vec3)b2.getPos(); // grabs the current position of Body 2
+		model_2 = glm::translate(model_2, pos2); // translates model matrix using the current position of Body 2
+		myShader.setMat4("model", model_2); // sets the calculated model matrix for Body 2 to the model matrix called within the vertex shader
+
+		glDrawArrays(GL_TRIANGLES, 0, 36);  // Updates the draw array with the current values for Body 2 set in the shader
 
 		// Imgui Render
 		ImGui::Render();
@@ -234,48 +365,28 @@ int main(int, char**)
 	}
 
 	// GLFW Set
-	glfwMakeContextCurrent(window); // sets the context of the window to current on the thread
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // sets the framebuffer size callback GLFW will use to the one I programmed
 	glfwSetErrorCallback(glfw_error_callback); // sets the error callback GLFW will use to the one I programmed
 
-	// Setup ImGui context
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls    
-
-	// Setup Platform/Renderer backends
-	ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
-	ImGui_ImplOpenGL3_Init(glsl_version);
-
-	// Initialises GLAD
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialise GLAD" << std::endl;
-		return -1;
-	}
-
-	ldvec3 pos1{0.0L, 0.0L, 0.0L};
-	ldvec3 v1{0.0L, 0.0L, 0.0L};
+	// Body 1 Characteristics
+	ldvec3 pos1{ 0.0L, 0.0L, 0.0L };
+	ldvec3 v1{ 0.0L, 0.0L, 0.0L };
 	long double m1 = 1;
 
-	ldvec3 pos2{ 0.8L, 0.0L, 0.0L };
-	ldvec3 v2{ 0.0L, 1.5L * M_PI, 0.0L };
-	long double m2 = 3.003e-6;
+	// Body 2 Characteristics
+	ldvec3 pos2{ 1.0L, 0.0L, 0.0L };
+	ldvec3 v2{ 0.0L, 2.0L * M_PI, 0.0L };
+	long double m2 = 1;
 
+	celestial_body sun(pos1, v1, m1);
+	celestial_body earth(pos2, v2, m2);
 
-	celestial_body sun = celestial_body(pos1, v1, m1);
-	celestial_body earth = celestial_body(pos2, v2, m2);
-
-	RK45_integration integrator = RK45_integration(1e-8, 1e-10, 0.05);
+	RK45_integration integrator(1e-8, 1e-10, 0.05);
 
 	bool show = true;
 	glm::vec4 background(0.5f, 0.5f, 0.5f, 1.0f);
 
-	celestial_body b1 = celestial_body(pos1, v1, m1);
-	celestial_body b2 = celestial_body(pos2, v2, m2);
-
-	buffer_box bufbx = buffer_box(b1, b2);
+	buffer_box bufbx = buffer_box(sun, earth);
 
 	bufbx.debugBackBuffer();
 	bufbx.debugFrontBuffer();
@@ -287,12 +398,22 @@ int main(int, char**)
 
 	int FPS = 60;
 
-	rendering_thread(window, FPS, background, show);
+	//rendering_thread(window, FPS, background, show, bufbx);
+
+	integrator.setDebug(true);
 
 	// Threading
-	std::thread p(physics_thread, window, integrator, bufbx);
-	std::thread r(rendering_thread, window, FPS, background, show);
+	std::thread p(physics_thread, window, std::ref(integrator), std::ref(bufbx), 1.0f);
+	//std::thread r(rendering_thread, window, FPS, background, show, std::ref(bufbx), glsl_version);
+
+	rendering_thread(window, FPS, background, show, bufbx, glsl_version);
+
+	p.join();
+	//r.join();
+
 };
+
+
 
 // Function definitions ~ kept below the main loop for formatting
 static void glfw_error_callback(int error, const char* description)
@@ -302,6 +423,7 @@ static void glfw_error_callback(int error, const char* description)
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+	glfwGetWindowSize(window, &SCR_HEIGHT, &SCR_WIDTH);
 	glViewport(0, 0, width, height); // Details how OpenGL should map its NDC (Normalised Device Coordinates) to the display
 }
 
---
+++ OpenGL-GR-Grav-Sim/shader.fs
@@ -0,0 +1,8 @@
+#version 330 core
+out vec4 FragColour;
+
+in vec4 vertexColour;
+
+void main() {
+	FragColour = vertexColour;
+}
